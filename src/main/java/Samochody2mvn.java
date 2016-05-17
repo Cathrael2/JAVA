497954c53989b17180b93c333779b542d93cac80
Bartek <barcie4@gmail.com>
2016-05-18 00:14:40 +0200

    Ver 1.0

--- src/main/java/Samochody2mvn.java
+++ src/main/java/Samochody2mvn.java
@@ -5,12 +5,11 @@ public class Samochody2mvn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Fabryka fabryka = new Fabryka();
+
 		
 		my_interface Interf = new my_interface();
 		Samochod tablica;
-		/*message my_mess = new message(Interf.getUserOrder().toString());
-		my_mess.show_mess();*/
+
 		Hashtable<String, Integer> map = Interf.getUserOrder();
 		int sum = 0;
 		for (int f : map.values()) {
@@ -26,26 +25,6 @@ public class Samochody2mvn {
 		tablica.wypiszSam();
 		System.out.println();
 		}while(iter.hasNext());
-		
-
-		/*Samochod tablica;
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		Hashtable<String, Integer> map = new Hashtable<String, Integer>();
-		map.put("TYP1", 1);
-		map.put("TYP5", 3);
-		map.put("TYP3", 5);
-		fabs.stworzSamochody(map);
-		Iterator iter = fabs.getIterator();
-
-		do
-		{
-		tablica = iter.next();
-		tablica.wypiszSam();
-		System.out.println();
-		}while(iter.hasNext());
-*/
 		
 	}
 }
